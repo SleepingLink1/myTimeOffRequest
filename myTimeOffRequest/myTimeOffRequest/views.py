b7b409d307b78e6474ad8f2a6353a40cfd3e3d61
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-01-28 23:32:17 -0600

    created a login view and a home template to go with it. Also updated urls.py to reflect new path

--- myTimeOffRequest/myTimeOffRequest/views.py
+++ myTimeOffRequest/myTimeOffRequest/views.py
@@ -1,7 +1,30 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden, HttpResponse
 from .models import TimeOffRequest
+from django.shortcuts import render
+from django.views import View
 
+
+class LoginView(View):
+    def get(self, request):
+        # Render the login form
+        return render(request, 'home.html')
+
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return HttpResponse('Logged in successfully.')
+        else:
+            # Return an 'invalid login' error message.
+            return HttpResponse('Invalid username or password.')
+class TimeOffRequestView(View):
+    def get(self, request):
+        return render(request, 'time_off_request.html')
 @login_required
 def modify_time_off_request(request, request_id):
     time_off_request = TimeOffRequest.objects.get(id=request_id)
