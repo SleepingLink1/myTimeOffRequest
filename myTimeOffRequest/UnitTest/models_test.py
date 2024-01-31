153a021b1d51ff0aafbb998f8e1413ba8850b3f4
SleepyLink1 <88170820+SleepyLink1@users.noreply.github.com>
2024-01-31 07:56:04 -0600

    added a few unit tests

--- myTimeOffRequest/UnitTest/models_test.py
+++ myTimeOffRequest/UnitTest/models_test.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from datetime import datetime
+from datetime import datetime, timedelta
 from myTimeOffRequest.myTimeOffRequest.models import Employee, TimeOffRequest
+
 
 class TimeOffRequestModelTest(TestCase):
     def setUp(self):
@@ -19,3 +20,12 @@ class TimeOffRequestModelTest(TestCase):
     def test_time_off_request_str(self):
         expected_object_name = f'{self.time_off_request.start_date} - {self.time_off_request.end_date}'
         self.assertEqual(str(self.time_off_request), expected_object_name)
+
+    def test_validate_request(self):
+        self.assertTrue(self.time_off_request.validate_request())
+
+        self.time_off_request.end_date = self.time_off_request.start_date - timedelta(days=1)
+        self.time_off_request.save()
+
+        # Now validate_request() should return False
+        self.assertFalse(self.time_off_request.validate_request())
